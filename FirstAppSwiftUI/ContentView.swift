cc99c44af117aeffeca0877fc183806a04b3fa2d
Noizzzz-Alex <Noizzzz@yandex.ru>
2023-12-09 09:55:20 +0700

    first commit

--- FirstAppSwiftUI/ContentView.swift
+++ FirstAppSwiftUI/ContentView.swift
@@ -8,17 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var color: ObservableColor
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Светофор")
+            HStack {
+                RowSquare(section: color)
+            }
+        }
+        VStack {
+            Button("Change color") {
+                switch color.color {
+                case .red:
+                    color.color = .yellow
+                case .yellow:
+                    color.color = .green
+                case .green:
+                    color.color = .red
+                }
+        
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
 }
 
+enum TraficLight {
+    case red, yellow, green
+}
+
+final class ObservableColor: ObservableObject {
+    @Published var color: TraficLight = .red
+}
+
+struct RowSquare: View {
+    @ObservedObject var section = ObservableColor()
+
+    var body: some View {
+        VStack {
+            Rectangle()
+                .frame(width: 50, height: 50)
+                .foregroundStyle(section.color == .red ? .red : .white)
+                .border(.red, width: 3)
+            Rectangle()
+                .frame(width: 50, height: 50)
+                .foregroundStyle(section.color == .yellow ? .yellow : .white)
+                .border(.yellow, width: 3)
+            Rectangle()
+                .frame(width: 50, height: 50)
+                .foregroundStyle(section.color == .green ? .green : .white)
+                .border(.green, width: 3)
+        }
+    }
+}
+
 #Preview {
-    ContentView()
+    ContentView(color: ObservableColor())
 }
--- FirstAppSwiftUI/FirstAppSwiftUIApp.swift
+++ FirstAppSwiftUI/FirstAppSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FirstAppSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(color: ObservableColor())
         }
     }
 }
