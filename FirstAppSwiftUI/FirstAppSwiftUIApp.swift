babec734761f8fbc37dcf1a1c0015a2896b174b9
Noizzzz-Alex <Noizzzz@yandex.ru>
2023-12-09 08:31:40 +0700

    Initial Commit

---
+++ FirstAppSwiftUI/FirstAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppSwiftUIApp.swift
+//  FirstAppSwiftUI
+//
+//  Created by Александр Харлампов on 09.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
